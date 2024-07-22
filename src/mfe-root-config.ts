3e5efa0fb161245a3082e99a6733d5a19be46265
educbank <brendo.gomes@educbank.com.br>
2024-07-21 21:15:23 -0300

    feat: add initial root config for Single-SPA orchestrator

---
+++ src/mfe-root-config.ts
@@ -0,0 +1,11 @@
+import { registerApplication, start, LifeCycles } from "single-spa";
+
+// registerApplication({
+//   name: "@mfe/navbar",
+//   app: () => System.import<LifeCycles>("@mfe/navbar"),
+//   activeWhen: ["/"]
+// });
+
+start({
+  urlRerouteOnly: true,
+});
