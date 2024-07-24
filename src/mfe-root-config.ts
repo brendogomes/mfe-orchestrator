45df38f42ea82f309120f3bd3f71219aba1f8280
educbank <brendo.gomes@educbank.com.br>
2024-07-23 23:20:45 -0300

    feat: Registrando mfe-topbar

--- src/mfe-root-config.ts
+++ src/mfe-root-config.ts
@@ -1,10 +1,48 @@
 import { registerApplication, start, LifeCycles } from "single-spa";
 
-registerApplication({
-  name: "@mfe/sidebar",
-  app: () => System.import<LifeCycles>("@mfe/sidebar"),
-  activeWhen: ["/"] 
-});
+const mfeApps = [
+  {
+    name: '@mfe/sidebar',
+    activeWhen: location => location.pathname.startsWith('/'),
+    domElement: 'sidebar' 
+  },
+  {
+    name: '@mfe/topbar',
+    activeWhen: location => location.pathname.startsWith('/'),
+    domElement: 'topbar'
+  }
+]
+
+for (const { name, activeWhen, domElement } of mfeApps) {
+  registerApplication(
+    name,
+    async () => {
+      const module = await System.import(name);
+      if (!module.bootstrap || !module.mount || !module.unmount) {
+        throw new Error(`Module for ${name} does not export expected lifecycle functions.`);
+      }
+      return {
+        bootstrap: module.bootstrap,
+        mount: module.mount,
+        unmount: module.unmount,
+      };
+    },
+    activeWhen,
+    { domElement: document.getElementById(domElement) }
+  );
+}
+
+// registerApplication({
+//   name: "@mfe/sidebar",
+//   app: () => System.import<LifeCycles>("@mfe/sidebar"),
+//   activeWhen: ["/"] 
+// });
+
+// registerApplication({
+//   name: "@mfe/topbar",
+//   app: () => System.import<LifeCycles>("@mfe/topbar"),
+//   activeWhen: ["/"] 
+// });
 
 start({
   urlRerouteOnly: true,
